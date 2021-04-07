630919c85d6ac5534bf7a2cee15e1d31b188b232
alejo4373 <alejandrofranco@pursuit.org>
2021-04-07 00:20:41 -0400

    Pass bonus tests

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,22 @@
 const Game = ({ cards, handleHitMe }) => {
+  let getCardValue = (value) => {
+    let values = { 'KING': 10, 'QUEEN': 10, 'JACK': 10 }
+    return values[value] || parseInt(value)
+  }
+
+  let score = cards.reduce((total, card) => total + getCardValue(card.value), 0)
+  let message = null
+  if (score === 21) {
+    message = "Blackjack!"
+  } else if (score > 21) {
+    message = "Busted..."
+  }
+
   return (
     <div>
       {cards.map(card => <img src={card.image} alt={`${card.value} of ${card.suit} card`} />)}
       <button onClick={handleHitMe}>Hit Me!</button>
+      <p>{message}</p>
     </div>
   )
 }
