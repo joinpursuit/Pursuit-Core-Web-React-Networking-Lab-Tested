15be1fc90ce85d2adda273b0ed3438e413ef76f7
Durdona Djalilova <ddjalilova3@gmail.com>
2021-05-01 16:10:54 -0400

    halfway there

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
+import Menu from "./Menu"
+import Game from "./Game"
+import axios from "axios"
 
 import "./App.css";
 
 export default class App extends React.Component {
+  state={deckID: "", cards: []}
+
+  generateDeck = async() =>{
+    try {
+      const res = await axios.get("https://deckofcardsapi.com/api/deck/new/draw?count=2")
+      this.setState({deckID: res.data.deck_id, cards: res.data.cards})
+    } catch (error) {
+      console.log(error)
+      this.setState({deckID: [], cards: []})
+    }
+  }
+
+  hitMe = async() =>{
+    try {
+      const res = await axios.get("https://deckofcardsapi.com/api/deck/${deckId}/draw?count=1")
+      this.setState({deckID: res.data.deck_id, cards: res.data.cards})
+    } catch (error) {
+      console.log(error)
+      this.setState({deckID: [], cards: []})
+    }
+  }
+
   render() {
+    console.log(this.state)
+    const {deckID, cards} = this.state
     return (
       <div className="app">
-        <h1>Hello, world!</h1>
+            <h2>Blackjack</h2>
+        <Menu generateDeck={this.generateDeck} deckID={deckID}/>
+        <Game deckID={deckID} cards={cards}/>
       </div>
     );
   }
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-const Menu = () => {
-  return null;
-};
+class Menu extends React.Component {
+  state={input: ""}
 
+  handleChange = (e) => {
+    const myDeckID= e.target.value;
+    this.setState({ input: myDeckID });
+  }
+  
+  handleSubmit = () =>{
+
+  }
+  
+  render() {
+    const {generateDeck, deckID} = this.props
+    return (
+      <div>
+    
+        <button onClick = {generateDeck}>Generate Deck</button>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Input Existing Deck
+            <input type="text" value="" onChange={this.handleChange}/>
+          </label>
+            <button>Draw</button>
+        </form>
+      </div>
+    );
+  }
+}
 export default Menu;
