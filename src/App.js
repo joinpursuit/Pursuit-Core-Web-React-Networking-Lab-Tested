d4d5afa870b90d5dd6a56a81a264e01126a0009f
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-05-01 17:11:36 -0400

    created twoCards variable

--- src/App.js
+++ src/App.js
@@ -2,38 +2,39 @@ import React from "react";
 import axios from "axios";
 import "./App.css";
 import Menu from "./components/Menu";
-import Game from "./components/Game"
+import Game from "./components/Game";
 
 class App extends React.Component {
-  state = { deckID: "", cards: [] }
+  state = { deckID: "", cards: [] };
+
   generateDeck = async () => {
     try {
-    const res = await axios.get(`https://deckofcardsapi.com/api/deck/new/draw?count=2`)
-    const newDeck = res.data.deck_id
-    this.setState({deckID: newDeck})
-    // const twoCards =   
-    console.log(res.data.deck_id)
-  }catch (err) {
-      this.state({deckID:""})
+      const res = await axios.get(
+        `https://deckofcardsapi.com/api/deck/new/draw?count=2`
+      );
+      const newDeck = res.data.deck_id;
+      const twoCards = res.data.cards;
+      this.setState({ deckID: newDeck, cards: twoCards });
+      // console.log(twoCards)
+    } catch (err) {
+      this.state({ deckID: "" });
     }
-  }
-  getCard = () => {
-
-  }
+  };
+  getCard = () => {};
   render() {
     if (this.state.deckID === "") {
       return (
         <div className="app">
-          <Menu props={this.state} generateDeck={this.generateDeck}/>
+          <Menu props={this.state} generateDeck={this.generateDeck} />
         </div>
       );
     } else {
-      console.log(this.state)
+      console.log(this.state);
       return (
         <>
-        <Game />
+          <Game props={this.state} />
         </>
-      )
+      );
     }
   }
 }
