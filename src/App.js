c9a6f65145de3156e6ec392d3696ac1f204e6090
anjuhypp <anjuhypp@gmail.com>
2021-05-01 16:04:54 -0400

    complete menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
+import Menu from "./Menu";
+import Game from "./Game";
+import axios from "axios";
 
 import "./App.css";
 
 export default class App extends React.Component {
+  state = { deckId: "", cards: [] };
+
+  generateDeck = async () => {
+    try {
+      const res = await axios.get(
+        "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+      );
+      this.setState({ deckId: res.data.deck_id });
+    } catch (error) {
+      console.log(error);
+      this.setState({ deckId: "" });
+    }
+  };
+
+  drawCard = async (input) => {
+    try {
+      debugger;
+      this.setState({ deckId: input });
+      const res = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${input}/draw/?count=2`
+      );
+      debugger;
+    } catch (error) {
+      debugger;
+      console.log(error);
+    }
+  };
+
   render() {
+    console.log(this.state.deckId);
+    const { deckId } = this.state
     return (
       <div className="app">
-        <h1>Hello, world!</h1>
+        {!deckId ? <Menu generateDeck={this.generateDeck} drawCard={this.drawCard} /> : null}
+        <Game />
       </div>
     );
   }
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,26 @@
 import React from "react";
 
-const Menu = () => {
-  return null;
-};
+class Menu extends React.Component {
+  state = { input: "" };
+
+  handleChange = (e) => {
+    this.setState({ input: e.target.value });
+  };
+
+  render() {
+    const { generateDeck, drawCard } = this.props;
+    const { input } = this.state;
+
+    return (
+      <div>
+        <h1>Blackjack</h1>
+        <button onClick={generateDeck}>Generate Deck</button>
+          <label htmlFor="deckInput">Input Existing Deck</label>
+          <input onChange={this.handleChange} value={input} name="deckInput" />
+          <button onClick={() => drawCard(input)}>Draw</button>
+      </div>
+    );
+  }
+}
 
 export default Menu;
