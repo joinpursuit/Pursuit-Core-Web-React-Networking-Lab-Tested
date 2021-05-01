e598105628e43bca2d58d1908ee825f701da71c0
Demi <oluwademiladejobi@pursuit.org>
2021-05-01 15:03:28 -0400

    displayed deckID

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
+import Menu from "./Menu.js"
+import axios from "axios"
 
 import "./App.css";
 
 export default class App extends React.Component {
+  state = {
+    deckID:"",
+    cards:[]
+  }
+
+getDeck = async() =>{
+try{
+const deck = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+this.setState({deckID:deck.data.deck_id})
+debugger
+}
+catch(error){
+  console.log(error);
+}
+}
+
+drawCard = async() =>{
+  
+}
+
+
+
   render() {
+    const {deckID, cards} = this.state
     return (
       <div className="app">
-        <h1>Hello, world!</h1>
+        <h1>Blackjack</h1>
+        <Menu getDeck={this.getDeck}/>
+        <p>Deck ID: {deckID}</p>
       </div>
     );
   }
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const Menu = () => {
-  return null;
+const Menu = ({getDeck}) => {
+
+
+const handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <button onClick={getDeck}>Generate Deck</button>
+      <br></br>
+      <label htmlFor="input">Input Existing Deck</label>
+      <input id="input"/>
+      <button>Draw</button>
+    </form>
+  );
 };
 
 export default Menu;
