a8544558fcafdbdb3d2e5a55132269bd2e912c3a
Jenna Qian <jiayin1996@gmail.com>
2021-05-03 13:53:28 -0700

    Networking-lab

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Game = () => {
-  return null;
+const Game = ({deckID,userHand,generateOneCard}) => {
+  return (
+    <div>
+      <p>Deck ID:{deckID}</p>
+      {userHand.map((e) => {return <img src={e.image}/>})}
+      <button onClick={generateOneCard} >Hit Me!</button>
+    </div>
+  )
 };
 
 export default Game;
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Menu = () => {
-  return null;
+const Menu = ({deckID,handleInput,generateTwoCards,generateDeck}) => {
+  // const {deckID} = props
+  // console.log(deckID)
+  return (
+    <div>
+      <button onClick={generateDeck}>Generate Deck</button>
+      <label>Input Existing Deck</label>
+      <input type="text" id="my-deck-id" value={deckID} onChange={handleInput} placeholder="type here..."></input>
+      <button onClick={generateTwoCards}>Draw</button>
+    </div>
+  )
 };
 
 export default Menu;
