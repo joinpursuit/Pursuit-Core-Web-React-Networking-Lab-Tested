502bd9f8ee5c1e53da1927907d1f18098d4d363a
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-05-04 23:10:57 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import axios from "axios";
 import React from "react";
 
 import "./App.css";
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      deckId:"",
+    }
+  }
+  componentDidMount(){
+    this.getRandomDeck()
+  }
+  
+
+  getRandomDeck = async () => {
+    try{
+      
+      const {data} = await axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+     console.log(data.deck_id)
+     this.setState({
+       deckId: data.deck_id,
+     })
+    } catch (e) {
+    console.log(e)
+  }
+} 
+  
+
   render() {
+   
     return (
       <div className="app">
-        <h1>Hello, world!</h1>
+        <h1> Blackjack</h1>
+        <button onClick={this.getRandomDeck}>Generate Deck</button>
+        <br></br>
+        
+        <label> Input Existing Deck:</label>
+        <input type="text" />
+        
+        
+        
+        
+      
       </div>
     );
   }
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,11 @@
 import React from "react";
 
+
 const Menu = () => {
   return null;
 };
 
+
+
+ 
 export default Menu;
