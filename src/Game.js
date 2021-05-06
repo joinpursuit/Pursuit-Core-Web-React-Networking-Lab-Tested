1eea5685c2e106b9ef1e390f8a9abf49387c0f27
Jamee Hengari <jhengari@hotmail.com>
2021-05-06 05:44:46 -0400

    second network commit

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Game = () => {
-  return null;
-};
+export class Game extends Component {
+  render() {
+    const { draw, deckId, cards } = this.props;
+    const img = cards.map((card, i) => {
+      return <img key={i} src={card.image} alt="card-image" />;
+    });
+    return (
+      <div>
+        <p>Deck ID: {deckId}</p>
+        {img}
+        <br />
+        <button onClick={draw}>Hit Me!</button>
+      </div>
+    );
+  }
+}
 
 export default Game;
--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Menu = () => {
-  return null;
-};
+export class Menu extends Component {
+  render() {
+    const { inputDraw, getDeck, handleChange, input } = this.props;
+    return (
+      <div>
+        <br />
+        <br />
+        <button onClick={getDeck}>Generate Deck</button>
+        <br />
+        <label>Input Existing Deck</label>
+        <input onChange={handleChange} value={input} type="text" />
+        <button onClick={inputDraw}>Draw</button>
+      </div>
+    );
+  }
+}
 
 export default Menu;
