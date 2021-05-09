123ae380e9909b94022427ada98e6e3a933320ff
Ivan Castillo <total180training@gmail.com>
2021-05-09 07:23:12 -0400

    Great lab for learning. Completed all the requirements. Might do bonus if time permits.

--- src/Game.js
+++ src/Game.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 
 export default class Game extends Component {
 
-
-
   render() {
+
+    const {deckId, cards, handleHitMe} = this.props
+    const displayCards = cards.map((card) => {
+      return <img src={card} alt="Playing Card" />
+    })
+
     return (
       <div>
-        
+        <p>Deck ID: {deckId}</p>
+        <div className='cardTable'>
+          {displayCards}
+        </div>
+        <button onClick={handleHitMe}>Hit Me!</button>
       </div>
     )
   }
-  
 }
--- src/Menu.js
+++ src/Menu.js
@@ -6,20 +6,24 @@ export default class Menu extends Component {
   
   
   render() {
+
+    const {deckId} = this.props
+
     return (
       <div>
-        This button needs to make the call + render the cards
         <button onClick={this.props.generateNewDeck}>Generate Deck</button>  
-        <p>New deck ID: {this.props.deckId}</p>
+        <p>Deck ID: {deckId}</p>
         <h2>Input Existing Deck</h2>
         <form onSubmit={this.props.handleSubmit} action="">
           <input onChange={this.props.handleChange} type="text" />
-          <button type='submit' >Draw</button>
+          <button type='submit'>Draw</button>
         </form>
       </div>
     )
   }
 
 }
+
+
 // Setting our value to the deckId prop state sets this form to controlled (or uncontrolled?) state
 // value={this.props.deckId}
