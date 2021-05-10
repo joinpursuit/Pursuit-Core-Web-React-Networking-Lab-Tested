f41a00715a4514899ae8a64105849d6b542077df
poonam <poonam398@gmail.com>
2021-05-10 18:48:34 -0400

    updated input feature

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,32 @@
+import axios from "axios";
 import React from "react";
 
-const Game = () => {
-  return null;
-};
+class Game extends React.Component {
+  
+  // state= {urls: []}
 
+
+  render() {
+    const {deckId, cards, hitMe} = this.props;
+    console.log(cards)
+
+  
+    const cardImg = cards.map(card =><img src={card.image} alt="card"/>)
+    console.log(cardImg)
+    return (
+      <div>
+        <h1>Blackjack</h1>
+        <p>Deck ID: {deckId} </p>
+
+        {cardImg}
+
+        <br/>
+
+        <button onClick={hitMe}>Hit Me!</button>
+
+       
+      </div>
+    );
+  }
+}
 export default Game;
