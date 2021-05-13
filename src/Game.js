e4f5df1002972501bff86e3a8fce9e6c4ef52215
chidalgo1221 <carlos.hidalgo2017@mesacharter.org>
2021-05-12 20:29:39 -0400

    Finished

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-const Game = () => {
-  return null;
+const Game = ({ deckId, cards, hitMe }) => {
+
+  return (
+    <section>
+      <p>Deck ID: {deckId}</p>
+      {cards.map(card => {
+        return <img src={card.image}/>
+      })}
+      <button onClick={hitMe}>Hit Me!</button>
+    </section>
+  );
 };
 
 export default Game;
