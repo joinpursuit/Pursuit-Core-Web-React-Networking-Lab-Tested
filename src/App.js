662113c3db2823670f94c4e4fef4ff27bfd75ca0
Flower Aviles <floweraviles@flowers-mini.myfiosgateway.com>
2021-05-19 18:37:42 -0400

    updated to hooks

--- src/App.js
+++ src/App.js
@@ -1,60 +1,61 @@
-import React from "react";
+import {useState, useEffect, useRef} from "react";
 import Menu from "./Menu";
 import "./App.css";
 import axios from "axios";
 import Game from "./Game";
 
-export default class App extends React.Component {
-  state = { deckId: "", cards: []};
+const App = () => {
+  const [deckId, setDeckId] = useState("")
+  const [cards, setCards] = useState([])
+  
+  useEffect(() => {
+    const generateDeck = async () => {
+      try {
+        const res = await axios.get(
+          "https://deckofcardsapi.com/api/deck/new/draw?count=2"
+        );
+          setDeckId(res.data.deck_id);
+          setCards(res.data.cards,);
+      } catch (error) {
+        console.log(error);
+        setDeckId("");
+        setCards([])
+      }
+    };
+  })
+  // handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   try {
+  //     const { deckId } = this.state
+  //     const res = await axios.get(
+  //       `https://deckofcardsapi.com/api/deck/${deckId}/draw?count=2`
+  //     );
+  //     debugger
+  //     this.setState({cards: res.data.cards})
+  //   } catch (error) {
+  //     console.log(error);
+  //     this.setState({ cards: [] });
+  //   }
+  // };
 
-  generateDeck = async () => {
-    try {
-      const res = await axios.get(
-        "https://deckofcardsapi.com/api/deck/new/draw?count=2"
-      );
-      this.setState({
-        deckId: res.data.deck_id,
-        cards: res.data.cards,
-      });
-    } catch (error) {
-      console.log(error);
-      this.setState({ deckId: "", cards: [] });
-    }
-  };
+  
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const { deckId } = this.state
-      const res = await axios.get(
-        `https://deckofcardsapi.com/api/deck/${deckId}/draw?count=2`
-      );
-      debugger
-      this.setState({cards: res.data.cards})
-    } catch (error) {
-      console.log(error);
-      this.setState({ cards: [] });
-    }
-  };
+  // handleChange = (e) => {
+  //   this.setState({ deckId: e.target.value });
+  // };
 
-  com
+  // hitMe = async () => {
+  //   try {
+  //     const { deckId } = this.state
+  //     const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw?count=1`)
+  //     this.setState((prevState) => ({cards: [...prevState.cards, ...res.data.cards]}))
+  //   } catch (error) {
+  //     console.log(error);
+  //     this.setState((prevState) => ({cards: prevState.cards}))
+  //   }
+  // }
 
-  handleChange = (e) => {
-    this.setState({ deckId: e.target.value });
-  };
-
-  hitMe = async () => {
-    try {
-      const { deckId } = this.state
-      const res = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw?count=1`)
-      this.setState((prevState) => ({cards: [...prevState.cards, ...res.data.cards]}))
-    } catch (error) {
-      console.log(error);
-      this.setState((prevState) => ({cards: prevState.cards}))
-    }
-  }
-
-  render() {
+  
     const { deckId, cards} = this.state;
     return (
       <div className="app">
@@ -68,8 +69,10 @@ export default class App extends React.Component {
         generateDeck={this.generateDeck}
         handleSubmit={this.handleSubmit}
         /> 
-      }
+        }
       </div>
     );
-  }
+  
 }
+
+export default App;
