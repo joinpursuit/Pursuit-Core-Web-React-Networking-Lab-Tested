8f7893dc6fdbfc4015ee1c5d5a4696d2a551f7a7
Talia Pichardo <taliapichardo@gmail.com>
2021-05-21 19:13:03 -0400

    passing all tests

---
+++ src/Components/Menu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Menu = ({ getCards, handleSubmit, deckId, handleChange }) => {
+    return (
+        <form onSubmit={handleSubmit}>
+            <button onClick={getCards}>Generate Deck</button>
+            <br />
+            <label htmlFor='my-deck-id'>Input Existing Deck</label>
+            <br />
+            <input onChange={handleChange} type='text' value={deckId} id='my-deck-id' />
+            <button>Draw</button>
+        </form>
+    )
+}
+
+export default Menu;
+
+
