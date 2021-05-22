3e1784e89720c58c761c7cf5b11093fc5dc55fbb
EsayH <ez@EsayHs-Mac-mini.local>
2021-05-22 00:22:00 -0400

    netwrk lab

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react'
 
-const Game = () => {
-  return null;
-};
+const Game = ({ cards, deckID, drawOne }) => {
+	return (
+		<div>
+			<h4>Deck ID: {deckID}</h4>
+			{cards.map(card => {
+				return <img src={card.image} alt='' key={card.code} />
+			})}
+			<button onClick={drawOne}>Hit Me!</button>
+		</div>
+	)
+}
 
-export default Game;
+export default Game
